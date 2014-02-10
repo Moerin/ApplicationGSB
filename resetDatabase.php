4df9ba4568451bdc405cac9ecd4212ae96b5385c
Moerin <trash.sebastien@gmail.com>
2014-02-10 01:02:11 +0100

    Création de la page de réinitialisation de base de donnée

--- resetDatabase.php
+++ resetDatabase.php
@@ -6,3 +6,49 @@
  * and open the template in the editor.
  */
 
+// Reinitialisation de la base de donnée
+
+$repInclude = './include/';
+require($repInclude . "_init.inc.php");
+
+// Efface les tables de la base données
+$mysqli = new mysqli("localhost", "userGsb", "secret", "gsb_frais");
+$mysqli->query('SET foreign_key_checks = 0');
+if ($result = $mysqli->query("SHOW TABLES"))
+{
+    while($row = $result->fetch_array(MYSQLI_NUM))
+    {
+        $mysqli->query('DROP TABLE IF EXISTS '.$row[0]);
+    }
+}
+
+$mysqli->query('SET foreign_key_checks = 1');
+$mysqli->close();
+
+// Nom du fichier conteant le script de creation de la base de donnée
+$filename = 'test.sql';
+
+$templine = '';
+// Lecture du fichier
+$lines = file($filename);
+
+foreach ($lines as $line) {
+    // Evite si c'est un commentaire
+    if (substr($line, 0, 2) == '--' || $line == '')
+        continue;
+
+        // Concatene les requètes
+            $templine .= $line;
+        // Si il y a un point virgule c'est que c'est la fin de la requète
+        if (substr(trim($line), -1, 1) == ';')
+        {
+            // Exécute la requète
+            mysql_query($templine, $idConnexion) or print('Error d\'execution de requete \'<strong>' . $templine . '\': ' . mysql_error() . '<br /><br />');
+            
+            $templine = '';
+        }
+}
+
+echo utf8_decode('Base de donnée reinitialisée');
+
+?>
