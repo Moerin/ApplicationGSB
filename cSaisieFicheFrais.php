8cc521b569b42f6d8e743a3defdc2016d8a3ce6f
Moerin <trash.sebatien@gmail.com>
2014-05-23 23:16:05 +0200

    Revert "Correction suppression ligne javascript"
    This reverts commit 1efc30ddfdad760232f9fa7d1e4724a2a378c006.

--- cSaisieFicheFrais.php
+++ cSaisieFicheFrais.php
@@ -140,10 +140,10 @@
                 <td><?php echo $lgEltHorsForfait["montant"] ; ?></td>
 
                 <td>
-                    <a href="?etape=validerSuppressionLigneHF&amp;idLigneHF=<?php echo $lgEltHorsForfait["id"]; ?>" 
+                    <img href="?etape=validerSuppressionLigneHF&amp;idLigneHF=<?php echo $lgEltHorsForfait["id"]; ?>" 
                          class="icon" title="Refuser la ligne hors forfait"
-                         onclick="return confirm('Voulez-vous vraiment supprimer cette ligne de frais hors forfait ?');">
-                        <img alt="icone Supprimer" src="images/reinitialiserIcon.png"></a>
+                         onclick="return confirm('Voulez-vous vraiment supprimer cette ligne de frais hors forfait ?');"
+                        alt="icone Supprimer" src="images/reinitialiserIcon.png">
                     <!--<a href="?etape=validerSuppressionLigneHF&amp;idLigneHF=<?php echo $lgEltHorsForfait["id"]; ?>"
                     onclick="return confirm('Voulez-vous vraiment supprimer cette ligne de frais hors forfait ?');"
                     title="Supprimer la ligne de frais hors forfait">Supprimer</a>--></td>
